4b3e49285c1f5bb1969bcae04c139c1ef1c343fa
Maxch26 <cam_he_max@hotmail.com>
2023-10-12 12:58:49 -0600

    practica08

---
+++ x.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practica06</title>
+    <link rel="stylesheet" href="./practica06.css">
+    <!-- <script defer src="./practica06.js"></script> -->
+</head>
+<body>
+    <h1>Inicio de sesion</h1>
+    <form action="" method="post" id="formulario">
+        <label for="user">Nombre de usuario:</label>
+        <input type="text" id="user" name="user">
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password">
+        <input type="submit" value="iniciar sesion" id="envio">
+    </form>
+    <script defer src="./practica06.js"></script>
+</body>
+</html>
